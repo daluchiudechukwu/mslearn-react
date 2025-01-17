22e991646f13bf40887aee835cd7de1c2c857ce3
Daluchi <133565272+daluchiudechukwu@users.noreply.github.com>
2025-01-17 14:34:44 +0100

    Delete build directory

--- build/dist/RecipeTitle.js
+++
@@ -1,7 +0,0 @@
-import React from "../_snowpack/pkg/react.js";
-function RecipeTitle() {
-  const title = "Mashed potatoes";
-  return /* @__PURE__ */ React.createElement("h2", null, title);
-}
-;
-export default RecipeTitle;
